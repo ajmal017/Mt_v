6f34c8fd15682e752a318048a188ca0c1436b5e8
pooria poorkarimi <Pooria_10000@hotmail.com>
2020-09-02 23:23:48 +0430

    Created registration form

--- Vorna/core/models.py
+++ Vorna/core/models.py
@@ -6,7 +6,6 @@ from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, \
 from django.conf import settings
 
 
-
 class UserManager(BaseUserManager):
 
     def create_user(self, email, password=None, **extra_fields):
@@ -27,17 +26,3 @@ class UserManager(BaseUserManager):
         user.save(using=self._db)
 
         return user
-        
-        
-class User(AbstractBaseUser, PermissionsMixin):
-    """Custom user model that supports using email instead of username"""
-    email = models.EmailField(max_length=255, unique=True)
-    name = models.CharField(max_length=255)
-    is_active = models.BooleanField(default=True)
-    is_staff = models.BooleanField(default=False)
-
-    objects = UserManager()
-
-    USERNAME_FIELD = 'email'
-    
-    
