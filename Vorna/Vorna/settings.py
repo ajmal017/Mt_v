5d92f52583eb517f10270c90ca0357995cc5c222
pooria poorkarimi <Pooria_10000@hotmail.com>
2020-09-05 20:23:16 +0430

    Added profile

--- Vorna/Vorna/settings.py
+++ Vorna/Vorna/settings.py
@@ -151,3 +151,4 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 LOGIN_REDIRECT_URL = 'home'
+LOGIN_URL = 'login'
--- Vorna/Vorna/urls.py
+++ Vorna/Vorna/urls.py
@@ -26,4 +26,5 @@ urlpatterns = [
     path('register/', user_views.register, name='register'),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
+    path('profile/', auth_views.LogoutView.as_view(template_name='users/profile.html'), name='profile'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
