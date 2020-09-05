2fdb4e2a0b678530e45ab0dc9b0182ff86937ff5
pooria poorkarimi <Pooria_10000@hotmail.com>
2020-09-05 20:33:53 +0430

    Edited profile

--- Vorna/Vorna/urls.py
+++ Vorna/Vorna/urls.py
@@ -24,7 +24,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('homepage.urls')),
     path('register/', user_views.register, name='register'),
+    path('profile/', user_views.as_view(template_name='users/profile.html'), name='profile'),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
-    path('profile/', auth_views.LogoutView.as_view(template_name='users/profile.html'), name='profile'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
