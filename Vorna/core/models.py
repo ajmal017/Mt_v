d15c6897e01237921ba4f906648363656772b6a3
pooria poorkarimi <Pooria_10000@hotmail.com>
2020-09-06 20:24:15 +0430

    Added profile Picture

--- Vorna/core/models.py
+++ Vorna/core/models.py
@@ -2,7 +2,7 @@ import uuid
 import os
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, \
-                                        PermissionsMixin
+                                        PermissionsMixin, User
 from django.conf import settings
 
 
@@ -26,3 +26,11 @@ class UserManager(BaseUserManager):
         user.save(using=self._db)
 
         return user
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    image = models.ImageField(default='default.jpeg', upload_to='profile_pics')
+
+    def __str__(self):
+        return f"{self.user.username} Profile"
