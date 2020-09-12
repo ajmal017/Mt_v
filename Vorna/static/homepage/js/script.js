8fef503e093b6242dbdcaa7f3e86d94513b0a2d2
pooria poorkarimi <Pooria_10000@hotmail.com>
2020-09-13 03:19:27 +0430

    Script jquery change

--- Vorna/static/homepage/js/script.js
+++ Vorna/static/homepage/js/script.js
@@ -1,62 +1,16 @@
 $(function() {
-	var dict = {
-		"row1": "EURUSD",
-		"row2": "GBPUSD",
-		"row3": "AUDUSD",
-		"row4": "USDCAD",
-		"row5": "USDJPY",
-		"row6": "USDINR",
-		"row7": "USDTRY",
-		"row8": "USDCNY",
-		"row9": "USDRUB",
-		"row10": "USDAED"
-	};
 
-
-	$("[name='price']").each(function(){
-			var ad = "https://www.markettime.ir/update/CurrencyExchange/"
-			// dict[$(this).attr("value")]
-			var el = $(this)
-			var t = $(this).text()
-			$.ajax({
-				url: ad,
-				type: 'GET',
-				async: true,
-				dataType: 'json',
-				success:function(data){
-					console.log(data)
-					$.each(data, function(index, json){
-
-					})
-					var after = parseFloat(data.rate).toFixed(4);
-					el.text(after);
-				}
-			});
-		});
-
-
-	setInterval(function(){
-		var ad = "https://www.markettime.ir/update/CurrencyExchange/"
-		// dict[$(this).attr("value")]
 		$.ajax({
 			url: ad,
 			type: 'GET',
 			async: true,
+			dataType: 'json',
 			success:function(data){
-
-			}
-		});
-		$("[name='price']").each(function(){
-			var ad = "https://www.markettime.ir/update/CurrencyExchange/latest/"
-			var el = $(this)
-			var t = $(this).text()
-			$.ajax({
-				url: ad,
-				type: 'GET',
-				async: true,
-				success:function(data){
-					var after = parseFloat(data.rate).toFixed(4);
-					var before = parseFloat(t).toFixed(4);
+				$.each(data, function(key,value) {
+					var el = $("#"+key);
+					var after = parseFloat(value).toFixed(4);
+					var before = parseFloat(el.text()).toFixed(4);
+					$("#" + key).text(after);
 					if (before != after) {
 						var sign = "";
 						if (after > before){
@@ -78,15 +32,53 @@ $(function() {
 									color: "white"
 								}, 1000)
 							})
-
 						}
 						var changed = parseFloat(Math.abs(after-before)).toFixed(4)
 						el.next().next().text(changed+sign)
-
 					}
-					el.text(after);
-				}
-			});
+				})
+			}
 		});
+	setInterval(function(){
+		var ad = "https://www.markettime.ir/update/CurrencyExchange/latest"
+		$.ajax({
+			url: ad,
+			type: 'GET',
+			async: true,
+			dataType: 'json',
+			success:function(data){
+				$.each(data, function(key,value) {
+					var el = $("#"+key);
+					var after = parseFloat(value).toFixed(4);
+					var before = parseFloat(el.text()).toFixed(4);
+					$("#" + key).text(after);
+					if (before != after) {
+						var sign = "";
+						if (after > before){
+							sign = "+";
+							el.animate({
+								color: "green"
+							}, 1000, function(){
+								el.animate({
+									color: "white"
+								}, 1000)
+							})
+
+						} else {
+							sign = "-";
+							el.animate({
+								color: "red"
+							}, 1000, function(){
+								el.animate({
+									color: "white"
+								}, 1000)
+							})
+						}
+						var changed = parseFloat(Math.abs(after-before)).toFixed(4)
+						el.next().next().text(changed+sign)
+					}
+			})
+		}
+	});
 	}, 3000);
 })
